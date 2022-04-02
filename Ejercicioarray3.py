840d683ed61981c51d33dcc3cedbdea2348e0d19
javierhuezo <huezosmyth@gmail.com>
2022-04-02 08:22:25 -0500

    Javier huezo
    Envio EjercicioFisica

---
+++ Ejercicioarray3.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  1 21:08:20 2022
+
+@author: ASUS
+"""
+
+nombres=[]
+apellidos=[]
+
+
+for x in range(3):
+   nom= input("Ingrese el  nombre: ")
+   nombres.append(nom)
+   
+   ape1= input("Ingrese el  primer apellido: ")   
+   ape2=input("Ingrese el segundo apellido: ")
+   apellidos.append([ape1,ape2])
+   print("****************************************")
+   
+for x in range(3):
+    print("Su nombre completo es: ", nombres[x],apellidos[x][0],apellidos[x] [1])
